7027f865348a3879134415627447833a8376505f
Sina <sina.programerr@gmail.com>
2023-09-23 19:37:15 +0330

    complete `initialize` phaze

--- main.py
+++ main.py
@@ -1,40 +1,78 @@
+from collections import namedtuple
+from src import game
 import random
-from src.game import Game
 
-flag = False
 
-def initializer(game: Game):   
-    strategic_nodes = game.get_strategic_nodes()['strategic_nodes']
-    score = game.get_strategic_nodes()['score']
-    strategic_nodes = list(zip(strategic_nodes, score))
-    strategic_nodes.sort(key=lambda x: x[1], reverse=True)
-    strategic_nodes, score = list(zip(*strategic_nodes))
-    print(game.get_turn_number())
-    owner = game.get_owners()
-    for i in strategic_nodes:
-        if owner[str(i)] == -1:
-            print(game.put_one_troop(i), "-- putting one troop on", i)
-            return 
-    adj = game.get_adj()
-    for i in strategic_nodes:
-        for j in adj[str(i)]:
-            if owner[str(j)] == -1:
-                print(game.put_one_troop(j), "-- putting one troop on neighbor of strategic node", j)
-                return
-    my_id = game.get_player_id()['player_id']
-    nodes = []
-    nodes.extend([i for i in strategic_nodes if owner[str(i)] == my_id])
-    for i in strategic_nodes:
-        for j in adj[str(i)]:
-            if owner[str(j)] == my_id:
-                nodes.append(j)
-    nodes = list(set(nodes))
-    node = random.choice(nodes)
-    game.put_one_troop(node)
-    print("3-  putting one troop on", node)
+Node = namedtuple('Node', ['id', 'score'])
+game.flag = False
+
+
+def keys_to_int(dic):
+    return {int(key): value for key, value in dic.items()}
+
+def put_one_troop(game, i, description=None):
+    response = game.put_one_troop(i)
+    print(f"One troop put in id={i}" + f" ({description})" if description else '')
+    print(response)
+
+def get_strategic_nodes(game):
+    strategic_nodes_ = game.get_strategic_nodes()['strategic_nodes']
+    scores_ = game.get_strategic_nodes()['score']
+    strategic_nodes = list(map(lambda x: Node(*x), zip(strategic_nodes_, scores_)))
+    strategic_nodes.sort(key=lambda node: node.score, reverse=True)
+    return strategic_nodes
+
+
+def initializer(game: game.Game):   
+    print('-'*50)
+    print('Turn: ', game.get_turn_number()['turn_number'])
+
+    strategic_nodes = get_strategic_nodes(game)
+    strategic_nodes_ = list(map(lambda node: node.id, strategic_nodes))
+    player_id = game.get_player_id()['player_id']
+    owners = keys_to_int(game.get_owners())
+    adjacents = keys_to_int(game.get_adj())
+    my_nodes = []
+
+    level = 1
+    checked_nodes = set()
+    checking_nodes = strategic_nodes_.copy()
+    while checking_nodes:
+        new_checking_nodes = []
+
+        for i in checking_nodes:
+            if i not in checked_nodes:
+                checked_nodes.add(i)
+                neighbors = adjacents[i]
+                new_checking_nodes.extend(set(neighbors) - checked_nodes)
+                owner = owners[i]
+
+                if owner in [player_id, -1]:
+                    my_nodes.append(i)
+
+                if owner == -1:
+                    put_one_troop(game, i, description=f"level={level}")
+                    return
+
+        checking_nodes = list(set(new_checking_nodes))
+        level += 1
+
+
+    troops_count = keys_to_int(game.get_number_of_troops())
+    my_strategic_nodes = list(filter(lambda i: i in strategic_nodes_, my_nodes))
+    my_ordinary_nodes = list(filter(lambda i: i not in strategic_nodes_, my_nodes))
+
+    for i in my_strategic_nodes:
+        if troops_count[i] < 3:
+            put_one_troop(game, i, description=f'strategic randomly')
+            return
+
+    i = random.choice(my_ordinary_nodes)
+    put_one_troop(game, i, description=f'ordinary randomly')
+    return
+
 
 def turn(game):
-    global flag
     print(game.get_number_of_troops_to_put())
     owner = game.get_owners()
     for i in owner.keys():
